80806e8b881c6827229156f34af7a953d8e28afa
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-06-10 10:31:13 -0300

    Scope and Interpolation

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$log', function($scope, $log){
-    $log.info($scope);
+myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout){
+
+    $scope.name = 'Tony';
+
+    $timeout(function(){
+        $scope.name = 'Everybody'
+    }, 3000);
+    
 }]);
