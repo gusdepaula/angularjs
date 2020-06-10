182009df6945bc4fba90e1a3880e1422bdbb058f
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-06-10 10:20:56 -0300

    Dependency Injection and Minification

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope, $log, $filter, $resource) {
-
-    console.log($resource);
-
-});
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log){
+    $log.info($scope);
+}]);
