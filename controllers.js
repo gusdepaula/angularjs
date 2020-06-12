8989228399dd2aa76c425877f28074c1883df9c1
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-06-12 17:44:45 -0300

    A Custom Directive

---
+++ controllers.js
@@ -0,0 +1,38 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+
+    $scope.appID = '3ad1ebb14d51169352930f82aceea563'; //key was gerenated in openweathermap.org
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, appid: $scope.appID, cnt: $scope.days });
+
+    console.log($scope.weatherResult);
+    
+    $scope.convertToCelsius = function(degK) {
+        
+        return Math.round((degK - 273));
+        
+    }
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
+    
+}]);
