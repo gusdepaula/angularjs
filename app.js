3732c1dd1ee824f492af558db6a2d6fcbc95c1a9
Gustavo de Paula Santos <gustavo.paula@certisign.com.br>
2020-06-12 15:53:21 -0300

    Binding Data (Part 1)

--- app.js
+++ app.js
@@ -25,15 +25,20 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     $scope.$watch('city', function(){
         cityService.city = $scope.city
     })
+    
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', function($scope, cityService, $resource){
 
     $scope.city = cityService.city;
 
-    $scope.$watch('city', function(){
-        cityService.city = $scope.city
-    })
+    $scope.weatherAPI = $resource("https://openweathermap.org/data/2.5/forecast/daily", 
+    {callback:"JSON_CALLBACK"}, 
+    { get: {method: "JSONP"}}, "&appid=3ad1ebb14d51169352930f82aceea563");
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, appid: 'ad1ebb14d51169352930f82aceea563'});
+
+    console.log($scope.weatherResult);
 
 }]);
